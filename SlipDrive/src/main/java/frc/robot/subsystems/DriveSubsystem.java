c21b165b12a8cc6f654203b895b4bc3f9f5a38c5
Zoob77391 <58796919+Zoob77391@users.noreply.github.com>
2022-02-28 19:32:05 -0500

    Drive subsystem

--- SlipDrive/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ SlipDrive/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,12 +4,30 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public DriveSubsystem() {}
+  private WPI_TalonFX frontLeft = new WPI_TalonFX(Constants.LeftFrontWheel);
+  private WPI_TalonFX backLeft = new WPI_TalonFX(Constants.LeftBackWheel);
+  private WPI_TalonFX frontRight = new WPI_TalonFX(Constants.RightFrontWheel);
+  private WPI_TalonFX backRight = new WPI_TalonFX(Constants.RightBackWheel);
+  private MecanumDrive mecanumDrive;
+    /** Creates a new ExampleSubsystem. */
+  public DriveSubsystem() {
+    mecanumDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
+    frontRight.setInverted(true);
+    backRight.setInverted(true);
 
+  }
+
+  public void drive(double y, double x, double z){
+    mecanumDrive.driveCartesian(y, x, z);
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
