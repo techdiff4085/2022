7e3f743cf53c0f1ace6bd5ca386262f25b941b6f
Zoob77391 <58796919+Zoob77391@users.noreply.github.com>
2022-02-28 20:15:52 -0500

    Intake Subsystem

--- SlipDrive/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ SlipDrive/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,11 +4,37 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
+  private WPI_VictorSPX rakeRight = new WPI_VictorSPX(Constants.RakeRight);
+  private WPI_VictorSPX rakeLeft = new WPI_VictorSPX(Constants.RakeLeft);
+  private WPI_VictorSPX horizontalRight = new WPI_VictorSPX(Constants.HorizontalRight);
+  private WPI_VictorSPX horizontalLeft = new WPI_VictorSPX(Constants.HorizontalLeft);
+  private DigitalInput upperLimitSwitch = new DigitalInput(Constants.UpperLimitSwitch);
+  private DigitalInput lowerLimitSwitch = new DigitalInput(Constants.LowerLimitSwitch);
+
   public IntakeSubsystem() {}
+
+  public void dropRake() {
+   //This lowers rakeRight and rakeLeft until it hits the lower limit switch.
+  }
+
+  public void liftRake() {
+    //This raises rakeRight and rakeLeft until it his the upper limit switch.
+  }
+
+  public void rakePull() {
+    //Starts horizonal motors.
+  }
+
+  public void rakeStop() {
+    //Stops horzontal motors.
+  }
 
   @Override
   public void periodic() {
