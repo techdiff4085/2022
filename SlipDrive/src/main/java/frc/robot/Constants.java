3f660d353d11391c41aa88abff998b7c0898f5d3
Deb Taylor <taylor0531@gmail.com>
2022-03-06 17:07:44 -0500

    added inner classes to constants.java

--- SlipDrive/src/main/java/frc/robot/Constants.java
+++ SlipDrive/src/main/java/frc/robot/Constants.java
@@ -14,21 +14,33 @@ package frc.robot;
  */
 public final class Constants {
 
-    public static final int LeftFrontWheel = 0;
-    public static final int LeftBackWheel = 0;
-    public static final int RightBackWheel = 0;
-    public static final int RightFrontWheel = 0;
-    public static final int elevatorLeft = 0;
-    public static final int elevatorMiddle = 0;
-    public static final int elevatorRight = 0;
-    public static final int shooter = 0;
-    public static final int RakeRight = 0;
-    public static final int RakeLeft = 0;
-    public static final int HorizontalRight = 0;
-    public static final int HorizontalLeft = 0;
-    public static final int UpperLimitSwitch = 0;
-    public static final int LowerLimitSwitch = 0;
-    public static final int Climb = 0;
-    public static final double KP = 0.05f;
-    public static final double KI = 0;
-    public static final double KD = 0.001;}
+    public static final class Motors {
+        public static final int LeftFrontWheel = 00;
+        public static final int LeftBackWheel = 0;
+        public static final int RightBackWheel = 0;
+        public static final int RightFrontWheel = 0;
+        public static final int elevatorLeft = 0;
+        public static final int elevatorMiddle = 0;
+        public static final int elevatorRight = 0;
+        public static final int shooter = 0;
+        public static final int RakeLiftLower = 0;
+        public static final int RakeIntake = 0;
+        public static final int HorizontalRight = 0;
+        public static final int HorizontalLeft = 0;
+        public static final int UpperLimitSwitch = 0;
+        public static final int LowerLimitSwitch = 0;
+        public static final int Climb = 0;
+        public static final double KP = 0.05f;
+        public static final double KI = 0;
+        public static final double KD = 0.001;
+    }   
+    public static final class LimitSwitches{
+        public static final int RAKEUP = 0;
+        public static final int RAKEDOWN = 0;
+        public static final int SHOOT = 0;
+    }
+
+
+
+
+}
